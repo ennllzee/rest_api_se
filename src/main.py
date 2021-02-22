ec7c1f0062de685998d78d06d39a2c25b458ed0f
ballyees <shouldertip@gmail.com>
2021-02-23 02:06:34 +0700

    v1

--- src/main.py
+++ src/main.py
@@ -9,6 +9,8 @@ from api.v1.BlueprintHistory.blueprintRoute import bp_history
 
 from sanic_cors import CORS
 
+import socket
+
 app = Sanic('daily_challenge')
 app.blueprint(bp_question)
 app.blueprint(bp_users)
@@ -20,13 +22,15 @@ async def test(request):
     return text(str(os.getpid()))
 
 if __name__ == "__main__":
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(hostname)
     # ssl = {'cert': "src/ert.pem", 'key': "src/key.pem"}
-    host = "192.168.1.37"
-    # host = "192.168.43.164"
+    # host = "192.168.1.37"
+    localhost = [(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]
     port = 8000
     try:
         # app.run(host=host, port=port, auto_reload=True, access_log=False)
-        app.run(host=host, port=port, auto_reload=True)
+        app.run(host=localhost, port=port, auto_reload=True)
         raise Exception('Stop server')
     except (Exception, KeyboardInterrupt, SystemExit) as e:
         print(e)
